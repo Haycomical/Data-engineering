392551734ead004fa019a1db195b2bc3e801ca58
Jothiswaran <125479273+Haycomical@users.noreply.github.com>
2025-07-28 15:24:54 +0530

    Create others.py

---
+++ 25 July/others.py
@@ -0,0 +1,16 @@
+#string
+movie = "The Lion King"
+print(movie[4:8])
+
+#list
+foods = ["Briyani", "Naan", "Butter chicken", "Burger"]
+foods.append("vada")
+print(foods)
+foods.remove("Naan")
+print(foods)
+for food in foods:
+    print(food)
+
+#tuple
+numbers = (10, 20, 30)
+print(numbers[-1])
